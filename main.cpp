2ae9b22d9eeb43aef5ca09271ec73c9e343f5124
asfa <asfasalsabilla@gmail.com>
2023-10-13 21:45:05 +0700

    menambah file main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "----------------------------------------------" << endl;
+    cout << "Nama : Asfa Salsabilla Sucipto\n" << endl;
+    cout << "NPM : 2310631170007\n" << endl;
+    cout << "Kelas : A - Informatika\n" << endl;
+    cout << "Cita Cita : Programmer\n" << endl;
+    cout << "Motivasi Masuk Informatika : Karena suka TIK\n" << endl;
+    cout << "----------------------------------------------" << endl;
+
+    return 0;
+}
