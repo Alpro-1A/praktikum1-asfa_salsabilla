93caea3bdd29c728246446fe0a9aa9138e615110
Asfa Salsabilla <147625545+AsfaSalsabilla@users.noreply.github.com>
2023-10-30 07:53:07 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,21 +4,42 @@ using namespace std;
 
 int main()
 {
-    int x, y, z;
-    cout << "masukkan angka pertama: ";
-    cin >> x;
-    cout << "masukkan angka kedua: ";
-    cin >> y;
-    cout << "masukkan angka ketiga: ";
-    cin >> z;
+    int pilihan;
+    cout << "ketik angka untuk bentuk bangun datar yang diinginkan: " <<endl;
+    cout << "1. Persegi" <<endl;
+    cout << "2. Persegi Panjang" <<endl;
+    cout << "3. Segitiga" <<endl;
+    cin >> pilihan;
+    int luas;
 
-    int terbesar = x;
-    if(y > terbesar){
-      terbesar = y;
-
-    } else if(z > terbesar){
-        terbesar = z;
+    switch (pilihan){
+       case 1:
+            int sisi;
+            cout << "Masukkan panjang sisi persegi: ";
+            cin >> sisi;
+            luas = sisi*sisi;
+            cout << "luas persegi adalah: "<< luas;
+            break;
+       case 2 :
+            int panjang, lebar;
+            cout << "masukkan panjang persegi panjang: ";
+            cin >> panjang;
+            cout << "masukkan lebar persegi panjang: ";
+            cin >> lebar;
+            luas = panjang*lebar;
+            cout << "luas persegi panjang adalah: " << luas;
+            break;
+       case 3 :
+            int alas, tinggi;
+            cout << "masukkan alas segitiga: ";
+            cin >> alas;
+            cout << "masukkan tinggi segitiga: ";
+            cin >> tinggi;
+            luas = 0.5*alas*tinggi;
+            cout << "luas segitiga adalah: " <<luas;
+            break;
+       default:
+            cout << "pilihan tidak valid." <<endl;
     }
-    cout << "angka terbesar adalah: " << terbesar << endl;
     return 0;
 }
